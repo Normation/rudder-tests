1ef0777e49fc5e2babc7d959b53f612c6a8b5735
Benoît Peccatte <benoit.peccatte@normation.com>
2015-06-02 16:23:46 +0200

    Fixes #6690: rtf broke after share and export merging

--- spec/tests/run_agent.rb
+++ spec/tests/run_agent.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 
-describe command('/var/rudder/cfengine-community/bin/cf-agent -KI ' + $params['PARAMS']) do
+describe command('/var/rudder/cfengine-community/bin/cf-agent -KI ' + $params['PARAMS'] + ' | grep -v "error: Warning promised, need to create file"') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should_not match /error:/ }
 end
