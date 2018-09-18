ec769f221c5890d4ae1b8a3093a94d898ca9add5
Felix Dallidet <felix.dallidet@normation.com>
2018-09-18 18:14:47 +0200

    Fixes #13503: Add support to private repository to rtf

--- scenario/ncf_tests.py
+++ scenario/ncf_tests.py
@@ -5,6 +5,7 @@ Parameters: TAG=4.3 define the ncf branch we want to test on this environment.
 """
 
 from scenario.lib import *
+import os
 import time
 from pprint import pprint
 
@@ -15,13 +16,22 @@ start(__doc__)
 tag = get_param("tag", "")
 # Get CFEngine version used for the tests
 cfengine_version = get_param("cfengine_version", "")
+export_prefix = ""
+try:
+  download_user = os.environ['DOWNLOAD_USER']
+  download_password = os.environ['DOWNLOAD_PASSWORD']
+
+  if (download_user):
+    export_prefix = "export DOWNLOAD_USER=" + download_user + " DOWNLOAD_PASSWORD=" + download_password
+except:
+  export_prefix = ""
 
 # Get setup_ncf
 shell_on("agent", "wget -O /tmp/ncf-setup https://www.rudder-project.org/tools/ncf-setup", live_output=True)
 # Call setup_ncf
 if not cfengine_version:
-  shell_on("agent", "sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#branches/rudder/" + tag + " rudder-" + tag, live_output=True)
+  shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#branches/rudder/" + tag + " rudder-" + tag, live_output=True)
 else:
-  shell_on("agent", "sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#branches/rudder/" + tag + " " + cfengine_version, live_output=True)
+  shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#branches/rudder/" + tag + " " + cfengine_version, live_output=True)
 
 finish()
