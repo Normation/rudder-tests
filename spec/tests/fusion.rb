7e51c557f52a9a0f4c57327627c7922b8bd2e5db
Felix Dallidet <felix.dallidet@normation.com>
2019-02-01 15:33:07 +0100

    Fixes #14266: Add more values tested in the inventory in rtf

--- spec/tests/fusion.rb
+++ spec/tests/fusion.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 osname = $params['OSNAME']
 
+if osname.downcase().include? "red hat"
+  fullname = "redhat"
+else
+  fullname = osname
+end
+
+version = {
+  "debian" => "[0-9]+\.[0-9]+(.[0-9]+)*",
+  "ubuntu" => "[0-9]+\.[0-9]+(.[0-9]+)*",
+  "redhat" => "[0-9]+\.[0-9]+(.[0-9]+)*",
+  "centos" => "[0-9]+\.[0-9]+(.[0-9]+)*",
+  "suse"   => "[0-9]+",
+}
+
 describe command("/opt/rudder/bin/run-inventory --local=/tmp") do
   its(:exit_status) { should eq 0 }
 end
@@ -13,6 +27,12 @@ end
 
 describe command("sed -ne '/<HARDWARE>/,/<.HARDWARE>/p' /tmp/test.ocs") do
    its(:stdout) { should match /<OSNAME>(?i:#{osname}).*<.OSNAME>/ }
+end
+
+describe command("sed -ne '/<OPERATINGSYSTEM>/,/<.OPERATINGSYSTEM>/p' /tmp/test.ocs") do
+   its(:stdout) { should match /<NAME>(?i:#{osname}).*<.NAME>/ }
+   its(:stdout) { should match /<FULL_NAME>(?i:#{fullname}).*<.FULL_NAME>/ }
+   its(:stdout) { should match /<VERSION>(?i:#{version[osname]})<.VERSION>/ }
 end
 # >-rm -rf /tmp/x
 # >-mkdir /tmp/x
