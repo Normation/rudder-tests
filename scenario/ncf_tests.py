5e133803ad8762eed8bdfa4b1a9bf429d5347e85
Felix Dallidet <felix.dallidet@normation.com>
2019-05-15 15:33:49 +0200

    Fixes #14892: rtf should run ncf-setup on master branch instead of the given version if this one does not exists

--- scenario/ncf_tests.py
+++ scenario/ncf_tests.py
@@ -41,18 +41,22 @@ if re.match(r"^[0-9]+$", tag):
   print(pull_id)
   if redmine_branch == "master":
     branch_version = redmine_branch
-    agent_version = get_issue_rudder_version(issue)
+    agent_version = get_issue_rudder_version(issue) +"-nightly"
   else:
     ret = requests.get("http://www.rudder-project.org/release-info/rudder/versions/" + redmine_version + "/git_branch")
     branch_version = ret.text
     agent_version = redmine_version
 
-  test_shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-pr https://github.com/Normation/ncf.git#" + branch_version + " rudder-" + agent_version + " " + pull_id + " --testinfra", live_output=True)
+  test_shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-pr https://github.com/Normation/ncf.git#" + branch_version + " ci/rudder-" + agent_version + " " + pull_id + " --testinfra", live_output=True)
 # Testing a ncf version
 else:
   if not cfengine_version:
-    cfengine_version = "rudder-" + tag
-  test_shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#branches/rudder/" + tag + " " + cfengine_version + " --testinfra", live_output=True)
+    cfengine_version = "ci/rudder-" + tag
+  ret = requests.get("http://www.rudder-project.org/release-info/rudder/versions/" + tag + "/git_branch")
+  branch_version = ret.text
+  if branch_version == "master":
+    cfengine_version = cfengine_version + "-nightly"
+  test_shell_on("agent", export_prefix + ";sh /tmp/ncf-setup test-local https://github.com/Normation/ncf.git#" + branch_version + " " + cfengine_version + " --testinfra", live_output=True)
 
 # Re-dumping test results
 shell_on("agent", "find /tmp/tmp* -name \"test.log\" | xargs cat", live_output=True);
