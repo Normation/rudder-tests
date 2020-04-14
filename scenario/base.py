f44b262b798529aa70fb56d660a8412fdcf0db68
Felix Dallidet <felix.dallidet@normation.com>
2020-04-14 16:31:32 +0200

    Fixes #17116: Base scenario can to be terminate anymore

--- scenario/base.py
+++ scenario/base.py
@@ -9,51 +9,57 @@ from scenario.lib import *
 
 # test begins, register start time
 start(__doc__)
-username = "user_test"
-directive_id = str(uuid.uuid4())
 
-# Generic tests
-for host in scenario.nodes():
-  hostinfo = scenario.platform.hosts[host].info
-  osname = hostinfo['inventory-os'] if 'inventory-os' in hostinfo else ""
-  run(host, 'fusion', Err.CONTINUE, OSNAME=osname)
+class Scenario():
+  def __init__(self, data):
+    self.data = data
+    self.username = "user_test"
+    self.directive_id = str(uuid.uuid4())
 
-run_on("all", 'agent', Err.CONTINUE)
+  def run(self):
 
-# force inventory
-run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="update")
-run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
-run_on("agent", 'run_agent', Err.CONTINUE, PARAMS="inventory")
-run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
-run_on("server", 'run_agent', Err.CONTINUE, PARAMS="run")
+    # Generic tests
+    for host in scenario.nodes():
+      hostinfo = scenario.platform.hosts[host].info
+      osname = hostinfo['inventory-os'] if 'inventory-os' in hostinfo else ""
+      run(host, 'fusion', Err.CONTINUE, OSNAME=osname)
 
-# accept nodes
-for host in scenario.nodes("agent"):
-  run_retry_and_dump('localhost', 'agent_accept', 5, RudderLog.APACHE, ACCEPT=host)
+    run_on("all", 'agent', Err.CONTINUE)
 
-# Add a rule
-date0 = host_date('wait', Err.CONTINUE, "server")
-run('localhost', 'user_rule', Err.BREAK, NAME="Test User", GROUP="special:all", USERNAME=username, DIRECTIVE_ID=directive_id )
-for host in scenario.nodes("agent"):
-  wait_for_generation('wait', Err.CONTINUE, "server", date0, host, 20)
+    # force inventory
+    run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="update")
+    run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
+    run_on("agent", 'run_agent', Err.CONTINUE, PARAMS="inventory")
+    run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
+    run_on("server", 'run_agent', Err.CONTINUE, PARAMS="run")
 
-# Run agent
-run_on("server", 'run_agent', Err.CONTINUE, PARAMS="run")
-run_and_dump("relay", 'run_agent', Err.CONTINUE, RudderLog.APACHE, PARAMS="update")
-run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
-run_and_dump("agent", 'run_agent', Err.CONTINUE, RudderLog.APACHE, PARAMS="update")
-run_on("agent", 'run_agent', Err.CONTINUE, PARAMS="run")
+    # accept nodes
+    for host in scenario.nodes("agent"):
+      run_retry_and_dump('localhost', 'agent_accept', 5, RudderLog.APACHE, ACCEPT=host)
 
-# Test rule result
-run_on("agent", "user_test", Err.CONTINUE, USERNAME=username)
+    # Add a rule
+    date0 = host_date('wait', Err.CONTINUE, "server")
+    run('localhost', 'user_rule', Err.BREAK, NAME="Test User", GROUP="special:all", USERNAME=self.username, DIRECTIVE_ID=self.directive_id )
+    for host in scenario.nodes("agent"):
+      wait_for_generation('wait', Err.CONTINUE, "server", date0, host, 20)
 
-# remove rule/directive
-run('localhost', 'directive_delete', Err.FINALLY, DELETE="Test User Directive")
-run('localhost', 'rule_delete', Err.FINALLY, DELETE="Test User Rule")
+    # Run agent
+    run_on("server", 'run_agent', Err.CONTINUE, PARAMS="run")
+    run_and_dump("relay", 'run_agent', Err.CONTINUE, RudderLog.APACHE, PARAMS="update")
+    run_on("relay", 'run_agent', Err.CONTINUE, PARAMS="run")
+    run_and_dump("agent", 'run_agent', Err.CONTINUE, RudderLog.APACHE, PARAMS="update")
+    run_on("agent", 'run_agent', Err.CONTINUE, PARAMS="run")
 
-# remove agent
-for host in scenario.nodes("agent"):
-  run('localhost', 'agent_delete', Err.FINALLY, DELETE=host)
+    # Test rule result
+    run_on("agent", "user_test", Err.CONTINUE, USERNAME=self.username)
 
-# test end, print summary
-finish()
+    # remove rule/directive
+    run('localhost', 'directive_delete', Err.FINALLY, DELETE="Test User Directive")
+    run('localhost', 'rule_delete', Err.FINALLY, DELETE="Test User Rule")
+
+    # remove agent
+    for host in scenario.nodes("agent"):
+      run('localhost', 'agent_delete', Err.FINALLY, DELETE=host)
+
+    # test end, print summary
+    finish()
