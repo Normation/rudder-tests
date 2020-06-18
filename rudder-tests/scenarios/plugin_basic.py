0afb5bf6b7c44d0e63cab243c21ad6bd9bfbf510
Felix Dallidet <felix.dallidet@normation.com>
2020-06-18 15:02:30 +0200

    Fixes #17785: Ad testinfra support to rudder-tests

--- rudder-tests/scenarios/plugin_basic.py
+++ rudder-tests/scenarios/plugin_basic.py
@@ -14,7 +14,6 @@ class plugin_basic(ScenarioInterface):
     plugin_path = self.input["plugin-path"]
     plugin_name = self.input["plugin-name"]
     plugin_version = self.input["plugin-version"]
-    server = self.nodes("server")[0]
 
-    self.run("localhost", 'plugin_basic', PLUGIN_PATH=plugin_path, PLUGIN_NAME=plugin_name, PLUGIN_VERSION=plugin_version)
+    self.run_testinfra("localhost", 'plugin_basic', PLUGIN_PATH=plugin_path, PLUGIN_NAME=plugin_name, PLUGIN_VERSION=plugin_version)
     self.finish()
