3500c4a8681ae54518cb76c63b22a1c0ffb76c65
Felix Dallidet <felix.dallidet@normation.com>
2021-02-22 18:02:14 +0100

    Fixes #18936: rudder-tests should be callable from cli

--- rudder-tests/run.py
+++ rudder-tests/run.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Rudder test framework
+
+Usage:
+    rtf run <scenario> [--data=<datastate_file>] [--input=<input_file>]
+
+Options:
+    --data <datastate_file> Datastate json file describing the target infrastucture to run the scenario on
+    --input <input_file> Input json file to fill scenario optional parameters
+
+Commands:
+    run
+       Run the given scenario on the target machines
+"""
 
 # Sample file, to try to build a basic schema for platform inputs
 
@@ -8,6 +22,7 @@ import re
 import sys
 import traceback
 from subprocess import Popen, PIPE
+from docopt import docopt
 from jsonschema import validate, draft7_format_checker, Draft7Validator, RefResolver
 import importlib
 
@@ -44,16 +59,22 @@ def run_scenario(name, datastate, scenario_input=None):
 
 
 ###################
+## MAIN
+if __name__ == "__main__":
+  input_file = "input.json"
+  data_file = "data.json"
+  args = docopt(__doc__)
+  if args['--input']:
+    input_file = args["--input"]
+  if args['--data']:
+    data_file = args["--data"]
 
-try:
-  with open("data.json", "r") as json_file:
-    jsonData = json.load(json_file)
-except Exception as e:
-    print("The data input seems malformed")
-    print(e)
-    exit(1)
-#scenario = run_scenario("test_user", jsonData, "input.json")
-#scenario = run_scenario("inventory", jsonData)
-#scenario = run_scenario("b078d18e_7a99_4bd5_8386_43eaf4f3669f", jsonData)
-scenario = run_scenario("cis", jsonData, "input.json")
+  try:
+    with open("data.json", "r") as json_file:
+      jsonData = json.load(json_file)
+  except Exception as e:
+      print("The data input seems malformed")
+      print(e)
+      exit(1)
+  scenario = run_scenario(args['<scenario>'], jsonData, "input.json")
 
