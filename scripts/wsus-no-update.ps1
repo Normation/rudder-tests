be5f7e39a85f0ecceecec401c7f63d0e7e6e0124
Félix Dallidet <felix.dallidet@rudder.io>
2022-06-08 01:14:15 +0200

    fixup! fixup! fixup! Fixes #21014: Add a wsus client setup script in rtf
    Fixes #21014: Add a wsus client setup script in rtf

--- scripts/wsus-no-update.ps1
+++ scripts/wsus-no-update.ps1
@@ -13,8 +13,8 @@ $reg = @(
  @{ Path = $wuRegPath; Key = "FillEmptyContentUrls"; Value = "1"; Type = "DWORD" },
  @{ Path = $wuRegPath; Key = "SetProxyBehaviorForUpdateDetection"; Value = "0"; Type = "DWORD" },
  @{ Path = $wuRegPath; Key = "UpdateServiceUrlAlternate"; Value = ""; Type = "String" },
- @{ Path = $wuRegPath; Key = "WUServer"; Value = "http://$server:8530"; Type = "String" },
- @{ Path = $wuRegPath; Key = "WUStatusServer"; Value = "http://$server:8531"; Type = "String" },
+ @{ Path = $wuRegPath; Key = "WUServer"; Value = "http://${server}:8530"; Type = "String" },
+ @{ Path = $wuRegPath; Key = "WUStatusServer"; Value = "http://${server}:8531"; Type = "String" },
 
  @{ Path = "${wuRegPath}/AU"; Key = "NoAutoUpdate"; Value = "1"; Type = "DWORD" },
  @{ Path = "${wuRegPath}/AU"; Key = "UseWUServer"; Value = "1"; Type = "DWORD" },
